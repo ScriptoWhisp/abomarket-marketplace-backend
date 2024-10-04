7342159e691ef26b08f83ecdb103fabf94d9de1a
ScriptoWhisp <daniel.tjulinov@gmail.com>
2024-10-04 11:23:16 +0300

    migrating to gradle

---
+++ src/test/java/ee/taltech/iti03022024project/Iti03022024ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti03022024project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iti03022024ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
