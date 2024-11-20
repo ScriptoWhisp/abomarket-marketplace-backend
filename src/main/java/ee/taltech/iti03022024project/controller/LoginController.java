f82ef1886b84a79298afa87fc58340f4cf2598e2
alexsm <alexsm@taltech.ee>
2024-11-20 18:12:39 +0200

    Finished logger implementation

--- src/main/java/ee/taltech/iti03022024project/controller/LoginController.java
+++ src/main/java/ee/taltech/iti03022024project/controller/LoginController.java
@@ -4,10 +4,12 @@ import ee.taltech.iti03022024project.security.LoginResponseDto;
 import ee.taltech.iti03022024project.security.LoginRequestDto;
 import ee.taltech.iti03022024project.service.LoginService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 public class LoginController {
@@ -16,6 +18,7 @@ public class LoginController {
 
     @PostMapping("/api/public/login")
     public LoginResponseDto login(@RequestBody LoginRequestDto request) {
+        log.info("Received login request: {}", request);
         return loginService.login(request);
     }
 }
