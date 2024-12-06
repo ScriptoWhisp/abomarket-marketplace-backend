62273ee6b01e5b27731d76d1333f94e6e6afc921
datjul <datjul@taltech.ee>
2024-12-06 17:16:34 +0200

    Roles

---
+++ src/main/java/ee/taltech/iti03022024project/domain/RoleEntity.java
@@ -0,0 +1,21 @@
+package ee.taltech.iti03022024project.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Schema(hidden = true)
+@ToString
+@Getter @Setter
+@Entity(name = "roles")
+public class RoleEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "role_id")
+        private int roleId;
+
+        private String role;
+}
---
+++ src/main/java/ee/taltech/iti03022024project/repository/RolesRepository.java
@@ -0,0 +1,8 @@
+package ee.taltech.iti03022024project.repository;
+
+import ee.taltech.iti03022024project.domain.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolesRepository extends JpaRepository<RoleEntity, Integer> {
+
+}
