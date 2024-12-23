acbe92f67f6d23d9198b6da7c0ab87d773ad0bdf
datjul <datjul@taltech.ee>
2024-12-23 17:50:33 +0200

    category pagination

--- src/main/java/ee/taltech/iti03022024project/repository/CategoryRepository.java
+++ src/main/java/ee/taltech/iti03022024project/repository/CategoryRepository.java
@@ -1,8 +1,11 @@
 package ee.taltech.iti03022024project.repository;
 
 import ee.taltech.iti03022024project.domain.CategoryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+    Page<CategoryEntity> findAllByCategoryNameContaining(String name, Pageable pageable);
 }
 
