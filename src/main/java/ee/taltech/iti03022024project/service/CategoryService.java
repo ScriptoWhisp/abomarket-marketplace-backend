9b50431314575bd02516766e041aa0921658d99d
datjul <datjul@taltech.ee>
2024-12-30 15:49:07 +0200

    optimize imports

--- src/main/java/ee/taltech/iti03022024project/service/CategoryService.java
+++ src/main/java/ee/taltech/iti03022024project/service/CategoryService.java
@@ -14,8 +14,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Slf4j
 @Service
 @Transactional
